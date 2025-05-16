f3456ed36fad213e85f3d9d6bd4824f46304ceca
NetiSanthoshKumar <santhoshkumarnethi544@gmail.com>
2025-05-16 19:28:14 +0530

    a12 added

---
+++ a12/index.js
@@ -0,0 +1,12 @@
+const products = [
+    {
+        id: 1,name : product 1,price:250
+    },
+    {
+        id: 2,name : product 2,price:500
+    },
+    {
+        id: 3,name : product 3,price:750
+    }
+];
+products.forEach((product) => console.log(products.name));
